1d50439b3f117266045189bdb121cb1931a244d9
Konstantin Chernyshkov <xchode@gmail.com>
2020-09-04 17:15:33 +0500

    basket and main improve

--- lib/basket.dart
+++ lib/basket.dart
@@ -9,14 +9,19 @@ class BasketWidget extends StatelessWidget {
       appBar: AppBar(
         title: Text("Корзина"),
       ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            // Navigate back to first screen when tapped.
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
-        ),
+      body: GridView.count(
+        // Create a grid with 2 columns. If you change the scrollDirection to
+        // horizontal, this produces 2 rows.
+        crossAxisCount: 1,
+        // Generate 100 widgets that display their index in the List.
+        children: List.generate(4, (index) {
+          return Center(
+            child: Text(
+              'Item $index',
+              style: Theme.of(context).textTheme.headline5,
+            ),
+          );
+        }),
       ),
       bottomNavigationBar: NavigationWidget(),
     );
